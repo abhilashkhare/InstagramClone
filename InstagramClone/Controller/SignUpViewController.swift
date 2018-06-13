fc3f67bd9d1fa1eb8284bf1e408e8812777bdec9
Abhilash Khare <abhilashkhare@gmail.com>
2018-06-12 19:02:40 -0700

    feat : Add Signup ViewController

---
+++ InstagramClone/Controller/SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewController.swift
+//  InstagramClone
+//
+//  Created by Abhilash Khare on 6/12/18.
+//  Copyright © 2018 Abhilash Khare. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
