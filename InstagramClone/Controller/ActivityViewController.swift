5cda8697335d623e111375ec75fc1f8027dc24a3
Abhilash Khare <abhilashkhare@gmail.com>
2018-06-18 08:53:44 -0700

    feat : Add ViewController class files for each tabbarController

---
+++ InstagramClone/Controller/ActivityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActivityViewController.swift
+//  InstagramClone
+//
+//  Created by Abhilash Khare on 6/18/18.
+//  Copyright © 2018 Abhilash Khare. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
