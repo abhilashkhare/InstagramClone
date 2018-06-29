0ff3e4cfcf8df5d54ed8c31f87fe09946f39401f
Abhilash Khare <abhilashkhare@gmail.com>
2018-06-28 23:56:01 -0700

    feat : Add functionality for mandatory text in UITextFields in SignupViewController

--- InstagramClone/Controller/SignUpViewController.swift
+++ InstagramClone/Controller/SignUpViewController.swift
@@ -17,7 +17,7 @@ class SignUpViewController: UIViewController,UIImagePickerControllerDelegate,UIN
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var profileImage: UIImageView!
-    
+    @IBOutlet weak var signUpButton: UIButton!
     var selectedImage : UIImage!
     var imagePicker = UIImagePickerController()
     
@@ -27,7 +27,26 @@ class SignUpViewController: UIViewController,UIImagePickerControllerDelegate,UIN
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectPhoto(tapGestureRecognizer:)))
         profileImage.isUserInteractionEnabled = true
         profileImage.addGestureRecognizer(tapGestureRecognizer)
+        handleTextFields()
     }
+      @objc func handleTextFields(){
+        userName.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+        emailTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+
+    }
+    
+    @objc  func textFieldDidChange(textField : UITextField){
+        guard  let userNameText = userName.text,!userNameText.isEmpty , let email = emailTextField.text,!email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            signUpButton.setTitleColor(UIColor.lightText, for: UIControlState.normal)
+            signUpButton.isEnabled = false
+            return
+        }
+        signUpButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        signUpButton.isEnabled = true
+    }
+    
+    
 
     @IBAction func signUpTouch(_ sender: Any) {
         Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authResultData, error) in
@@ -82,4 +101,6 @@ class SignUpViewController: UIViewController,UIImagePickerControllerDelegate,UIN
     @IBAction func selectPhoto(tapGestureRecognizer: UITapGestureRecognizer){
         self.present(imagePicker, animated: true, completion: nil)
     }
+    
+    
 }
