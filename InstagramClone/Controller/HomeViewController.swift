473fe1b2be04506ec046b292bf0e8774207dabb9
Abhilash Khare <abhilashkhare@gmail.com>
2018-07-05 18:50:23 -0700

    feat: Add Log out functionality

--- InstagramClone/Controller/HomeViewController.swift
+++ InstagramClone/Controller/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
@@ -20,16 +21,17 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+   
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutButton(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+        } catch {
+            print("Logout error")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    
 
 }
